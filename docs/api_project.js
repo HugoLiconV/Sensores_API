deacef381befc8112941245c165504bd40c2686d
Hugo <hugo_val28@hotmail.com>
2018-05-17 15:45:44 -0600

    added documentation

---
+++ docs/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "sensores-api",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2018-05-17T17:08:46.402Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
